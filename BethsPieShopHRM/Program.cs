8c9293a5205dac9c04de1d779db79a5877c861db
William Farmer <william.farmer@cardinus.com>
2024-07-23 11:53:27 +0100

    Complete C# Fundamentals Module 7

--- BethsPieShopHRM/Program.cs
+++ BethsPieShopHRM/Program.cs
@@ -14,5 +14,32 @@ bethany.PerformWork();
 bethany.PerformWork(5);
 bethany.PerformWork();
 
+bethany.firstName = "John";
+bethany.hourlyRate = 10;
+
+bethany.DisplayEmployeeDetails();
+bethany.PerformWork();
+bethany.PerformWork(12);
+bethany.PerformWork();
+bethany.ReceiveWage();
+
+string fn = bethany.firstName;
+
 double receivedWageBethany = bethany.ReceiveWage(true);
 Console.WriteLine($"Wage paid (Message from program): {receivedWageBethany}");
+
+Console.WriteLine("Creating an employee");
+Console.WriteLine("--------------------\n");
+//Here we are using new without specifying the type since it's known on the left-hand side
+
+Employee george = new("George", "Jones", "george@snowball.be", new DateTime(1984, 3, 28), 30);
+
+//Dan methods aanroepen
+george.DisplayEmployeeDetails();
+george.PerformWork();
+george.PerformWork();
+george.PerformWork(3);
+george.PerformWork();
+george.PerformWork(8);
+
+var receivedWageGeorge = george.ReceiveWage(true);
