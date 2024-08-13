af357d56a0ce76b853cf16a2a17838e6cf6721ed
William Farmer <william.farmer@cardinus.com>
2024-08-13 15:26:01 +0100

    C# Fundamentals Module 8 Demo Creating Enumerations

--- BethsPieShopHRM/Employee.cs
+++ BethsPieShopHRM/Employee.cs
@@ -1,12 +1,8 @@
 ﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BethsPieShopHRM
 {
+
 	internal class Employee
 	{
 		public string firstName;
@@ -21,18 +17,21 @@ namespace BethsPieShopHRM
 
 		const int minimalHoursWorkedUnit = 1;
 
-		public Employee(string first, string last, string em, DateTime bd) : this(first, last, em, bd, 0)
-		{
+		public EmployeeType employeeType;
 
+		public Employee(string first, string last, string em, DateTime bd) : this(first, last, em, bd, 0, 
+			EmployeeType.StoreManager)
+		{
 		}
 
-		public Employee(string first, string last, string em, DateTime bd, double rate)
+		public Employee(string first, string last, string em, DateTime bd, double rate, EmployeeType empType)
 		{
 			firstName = first;
 			lastName = last;
 			email = em;
 			birthDay = bd;
 			hourlyRate = rate;
+			employeeType = empType;
 		}
 
 		public void PerformWork()
@@ -103,7 +102,15 @@ namespace BethsPieShopHRM
 
 		public double ReceiveWage(bool resetHours = true)
 		{
-			wage = numberOfHoursWorked * hourlyRate;
+			if (employeeType == EmployeeType.Manager)
+			{
+				Console.WriteLine($"An extra was added to the wage since {firstName} is a manager!");
+				wage = numberOfHoursWorked * hourlyRate * 1.25;
+			}
+			else
+			{
+				wage = numberOfHoursWorked * hourlyRate;
+			}
 
 			Console.WriteLine($"{firstName} {lastName} has received a wage of {wage} for {numberOfHoursWorked} hour(s) of work.");
 
--- BethsPieShopHRM/Program.cs
+++ BethsPieShopHRM/Program.cs
@@ -12,7 +12,8 @@ using System.Text;
 Console.WriteLine("Creating an employee");
 Console.WriteLine("--------------------\n");
 
-Employee bethany = new Employee("Bethany", "Smith", "bethany@snowball.be", new DateTime(1979, 1, 16), 25);
+Employee bethany = new Employee("Bethany", "Smith", "bethany@snowball.be", new DateTime(1979, 1, 16), 25,
+	EmployeeType.Manager);
 
 string bethanyAsJson = bethany.ConvertToJson();
 Console.WriteLine(bethanyAsJson);
@@ -73,39 +74,40 @@ Console.WriteLine(bethanyAsJson);
 //int receivedBonus = bethany.CalculateBonusAndBonusTax(minimumBonus, out bonusTax);
 //Console.WriteLine($"The minimum bonus is {minimumBonus}, the bonus tax is {bonusTax} and {bethany.firstName} has received a bonus of {receivedBonus}");
 
-//bethany.DisplayEmployeeDetails();
+bethany.DisplayEmployeeDetails();
 
-//bethany.PerformWork();
-//bethany.PerformWork();
-//bethany.PerformWork(5);
-//bethany.PerformWork();
+bethany.PerformWork();
+bethany.PerformWork();
+bethany.PerformWork(5);
+bethany.PerformWork();
 
-//bethany.firstName = "John";
-//bethany.hourlyRate = 10;
+bethany.firstName = "John";
+bethany.hourlyRate = 10;
 
-//bethany.DisplayEmployeeDetails();
-//bethany.PerformWork();
-//bethany.PerformWork(12);
-//bethany.PerformWork();
-//bethany.ReceiveWage();
+bethany.DisplayEmployeeDetails();
+bethany.PerformWork();
+bethany.PerformWork(12);
+bethany.PerformWork();
+bethany.ReceiveWage();
 
-//string fn = bethany.firstName;
+string fn = bethany.firstName;
 
-//double receivedWageBethany = bethany.ReceiveWage(true);
-//Console.WriteLine($"Wage paid (Message from program): {receivedWageBethany}");
+double receivedWageBethany = bethany.ReceiveWage(true);
+Console.WriteLine($"Wage paid (Message from program): {receivedWageBethany}");
 
-//Console.WriteLine("Creating an employee");
-//Console.WriteLine("--------------------\n");
+Console.WriteLine("Creating an employee");
+Console.WriteLine("--------------------\n");
 ////Here we are using new without specifying the type since it's known on the left-hand side
 
-//Employee george = new("George", "Jones", "george@snowball.be", new DateTime(1984, 3, 28), 30);
+Employee george = new("George", "Jones", "george@snowball.be", new DateTime(1984, 3, 28), 30,
+	EmployeeType.Research);
 
 ////Dan methods aanroepen
-//george.DisplayEmployeeDetails();
-//george.PerformWork();
-//george.PerformWork();
-//george.PerformWork(3);
-//george.PerformWork();
-//george.PerformWork(8);
+george.DisplayEmployeeDetails();
+george.PerformWork();
+george.PerformWork();
+george.PerformWork(3);
+george.PerformWork();
+george.PerformWork(8);
 
-//var receivedWageGeorge = george.ReceiveWage(true);
+var receivedWageGeorge = george.ReceiveWage(true);
