1ab8291a9a8e0079ad4de9985a7ab8040466853c
William Farmer <william.farmer@cardinus.com>
2024-08-27 16:07:52 +0100

    Complete C# Fundamentals Module 9, Demo Working with null

---
+++ BethsPieShopHRM/HR/Employee.cs
@@ -0,0 +1,125 @@
+﻿using Newtonsoft.Json;
+
+namespace BethsPieShopHRM.HR
+{
+
+    internal class Employee
+    {
+        public string firstName;
+        public string lastName;
+        public string email;
+
+        public int numberOfHoursWorked;
+        public double wage;
+        public double? hourlyRate;
+
+        public DateTime birthDay;
+
+        const int minimalHoursWorkedUnit = 1;
+
+        public EmployeeType employeeType;
+
+        public static double taxRate = 0.15;
+
+        public Employee(string first, string last, string em, DateTime bd) : this(first, last, em, bd, 0,
+            EmployeeType.StoreManager)
+        {
+        }
+
+        public Employee(string first, string last, string em, DateTime bd, double? rate, EmployeeType empType)
+        {
+            firstName = first;
+            lastName = last;
+            email = em;
+            birthDay = bd;
+            hourlyRate = rate ?? 10;
+            employeeType = empType;
+        }
+
+        public void PerformWork()
+        {
+            //numberOfHoursWorked++;
+            PerformWork(minimalHoursWorkedUnit);
+            //Console.WriteLine($"{firstName} {lastName} has worked for {numberOfHoursWorked} hour(s)!");
+        }
+
+        public void PerformWork(int numberOfHours)
+        {
+            numberOfHoursWorked += numberOfHours;
+            Console.WriteLine($"{firstName} {lastName} has worked for {numberOfHours} hour(s)!");
+        }
+
+        public int CalculateBonus(int bonus)
+        {
+
+            if (numberOfHoursWorked > 10)
+                bonus *= 2;
+
+            Console.WriteLine($"The employee got a bonus of {bonus}");
+            return bonus;
+        }
+
+        public int CalculateBonusAndBonusTax(int bonus, out int bonusTax)
+        {
+            bonusTax = 0;
+            if (numberOfHoursWorked > 10)
+                bonus *= 2;
+
+            if (bonus >= 200)
+            {
+                bonusTax = bonus / 10;
+                bonus -= bonusTax;
+            }
+
+            Console.WriteLine($"The employee got a bonus of {bonus} and the tax on the bonus is {bonusTax}");
+            return bonus;
+        }
+
+        public static void UsingACustomType()
+        {
+            List<string> list = new List<string>();
+        }
+
+        public string ConvertToJson()
+        {
+            string json = JsonConvert.SerializeObject(this);
+            return json;
+        }
+
+		public double ReceiveWage(bool resetHours = true)
+		{
+			double wageBeforeTax = 0.0;
+
+			if (employeeType == EmployeeType.Manager)
+			{
+				Console.WriteLine($"An extra was added to the wage since {firstName} is a manager!");
+				wageBeforeTax = numberOfHoursWorked * hourlyRate.Value * 1.25;
+			}
+			else
+			{
+				wageBeforeTax = numberOfHoursWorked * hourlyRate.Value;
+			}
+
+			double taxAmount = wageBeforeTax * taxRate;
+
+			wage = wageBeforeTax - taxAmount;
+
+			Console.WriteLine($"{firstName} {lastName} has received a wage of {wage} for {numberOfHoursWorked} hour(s) of work.");
+
+			if (resetHours)
+				numberOfHoursWorked = 0;
+
+			return wage;
+		}
+
+		public static void DisplayTaxRate()
+		{
+			Console.WriteLine($"The current tax rate is {taxRate}");
+		}
+
+		public void DisplayEmployeeDetails()
+        {
+			Console.WriteLine($"\nFirst name: \t{firstName}\nLast name: \t{lastName}\nEmail: \t\t{email}\nBirthday: \t{birthDay.ToShortDateString()}\nTax rate: \t{taxRate}");
+		}
+    }
+}
--- BethsPieShopHRM/Program.cs
+++ BethsPieShopHRM/Program.cs
@@ -1,118 +1,61 @@
-﻿
-
-using BethsPieShopHRM;
-using System.Text;
-
-//int a = 42;
-//int aCopy = a;
-//Console.WriteLine($"Value of a: {a} and value of copy of a: {aCopy}");
-//aCopy = 100;
-//Console.WriteLine($"Value of a: {a} and value of copy of a: {aCopy}");
+﻿using BethsPieShopHRM.HR;
 
 Console.WriteLine("Creating an employee");
 Console.WriteLine("--------------------\n");
 
-Employee bethany = new Employee("Bethany", "Smith", "bethany@snowball.be", new DateTime(1979, 1, 16), 25,
-	EmployeeType.Manager);
+Employee bethany = new Employee("Bethany", "Smith", "bethany@snowball.be", new DateTime(1979, 1, 16), 25, EmployeeType.Manager);
 
-string bethanyAsJson = bethany.ConvertToJson();
-Console.WriteLine(bethanyAsJson);
+Console.WriteLine("Creating an employee");
+Console.WriteLine("--------------------\n");	//Here we are using new without specifying the type since it's known on the left-hand side
 
-//bethany.PerformWork(25);
+Employee george = new("George", "Jones", "george@snowball.be", new DateTime(1984, 3, 28), null, EmployeeType.Research);
 
-//string name = "bethany";
-//string anotherName = name;
-//name += " smith";
+Employee mysteryEmployee = null;
+mysteryEmployee.DisplayEmployeeDetails();
 
-//Console.WriteLine("Name: " + name);
-//Console.WriteLine("Another name: " + anotherName);
+#region First run Bethany
 
-//string upperCaseName = name.ToUpper();
-
-//Console.WriteLine("Name: " + name);
-//Console.WriteLine("Uppercase name: " +	upperCaseName);
-
-//string indexes = string.Empty;
-
-//for (int i = 0; i < 2500; i++)
-//{
-//	indexes += i.ToString();
-//}
-
-//string firstName = "Bethany";
-//string lastName = "Smith";
-
-//StringBuilder builder = new StringBuilder();
-
-//builder.Append("Last name: ");
-//builder.AppendLine(lastName);
-//builder.Append("First name: ");
-//builder.Append(firstName);
-//string result = builder.ToString();
-
-//StringBuilder builder2 = new StringBuilder();
-
-//for (int i = 0; i < 2500; i++)
-//{
-//	builder2.Append(i);
-//	builder2.Append(" ");
-//}
-
-//string list = builder2.ToString();
-
-//int minimumBonus = 100;
-//int receivedBonus = bethany.CalculateBonus(minimumBonus);
-//Console.WriteLine($"The minimum bonus is {minimumBonus} and {bethany.firstName} has received a bonus of {receivedBonus}");
-
-//int minimumBonus = 100;
-//int bonusTax = 0;
-//int receivedBonus = bethany.CalculateBonusAndBonusTax(minimumBonus, ref bonusTax);
-//Console.WriteLine($"The minimum bonus is {minimumBonus}, the bonus tax is {bonusTax} and {bethany.firstName} has received a bonus of {receivedBonus}");
-
-//int minimumBonus = 100;
-//int bonusTax;
-//int receivedBonus = bethany.CalculateBonusAndBonusTax(minimumBonus, out bonusTax);
-//Console.WriteLine($"The minimum bonus is {minimumBonus}, the bonus tax is {bonusTax} and {bethany.firstName} has received a bonus of {receivedBonus}");
-
-bethany.DisplayEmployeeDetails();
-
-bethany.PerformWork();
 bethany.PerformWork();
 bethany.PerformWork(5);
 bethany.PerformWork();
-
-bethany.firstName = "John";
-bethany.hourlyRate = 10;
-
+bethany.ReceiveWage();
 bethany.DisplayEmployeeDetails();
+
+#endregion
+
+
+#region First run George
+
+george.PerformWork(10);
+george.PerformWork();
+george.PerformWork();
+george.ReceiveWage();
+george.DisplayEmployeeDetails();
+
+#endregion
+
+Employee.taxRate = 0.02;//woohoo, less money to pay
+
+
+#region Second run Bethany
+
 bethany.PerformWork();
-bethany.PerformWork(12);
+bethany.PerformWork();
+bethany.PerformWork();
+bethany.PerformWork();
+bethany.PerformWork();
 bethany.PerformWork();
 bethany.ReceiveWage();
+bethany.DisplayEmployeeDetails();
 
-string fn = bethany.firstName;
+#endregion
 
-double receivedWageBethany = bethany.ReceiveWage(true);
-Console.WriteLine($"Wage paid (Message from program): {receivedWageBethany}");
+#region Second run George
 
-Console.WriteLine("Creating an employee");
-Console.WriteLine("--------------------\n");
-////Here we are using new without specifying the type since it's known on the left-hand side
-
-Employee george = new("George", "Jones", "george@snowball.be", new DateTime(1984, 3, 28), 30,
-	EmployeeType.Research);
-
-////Dan methods aanroepen
+george.PerformWork();
+george.PerformWork();
+george.PerformWork();
+george.ReceiveWage();
 george.DisplayEmployeeDetails();
-george.PerformWork();
-george.PerformWork();
-george.PerformWork(3);
-george.PerformWork();
-george.PerformWork(8);
 
-var receivedWageGeorge = george.ReceiveWage(true);
-
-WorkTask task;
-task.description = "Bake delicious pies";
-task.hours = 3;
-task.PerformWorkTask();
+#endregion
